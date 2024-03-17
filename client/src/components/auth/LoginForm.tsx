2cb0c32f5011b29c21a5ac541135f4a3f4c15202
ndnghiadn <ndnghiadn@gmail.com>
2024-03-17 19:27:38 +0700

    update styles

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 import { useUserStore } from "@/lib/zustand";
 import { User } from "@/constants/types";
 import { toast } from "sonner";
+import { Button } from "../ui/button";
 
 const LoginForm = () => {
   const router = useRouter();
@@ -126,16 +127,16 @@ const LoginForm = () => {
                   Forgot password?
                 </a>
               </div>
-              <button
+              <Button
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
-              </button>
+              </Button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{" "}
                 <a
-                  href="#"
+                  href="/register"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Sign up
